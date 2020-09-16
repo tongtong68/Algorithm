9d103ffc7c13b50c220b0b1ec4b95a87bb0493ab
Carter Peng <pengtong@infosec.com.cn>
2020-09-16 12:04:57 +0800

    添加自己的方法和另外两种算法

--- app/src/main/java/com/example/algorithm/MainActivity.java
+++ app/src/main/java/com/example/algorithm/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.algorithm;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.algorithm.easy.OneSum;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        testOneSum();
+    }
+
+    private void testOneSum() {
+        int[] array = {1, 8, 9, 3, 5, 6, 7, 3};
+        int target = 6;
+        OneSum oneSum = new OneSum(array, target);
+        List<int[]> result = oneSum.getSum();
+        Log.d("test", "=====size: " + result.size());
+        for (int i = 0 ; i<result.size();i++){
+            int[] temp = result.get(i);
+            Log.d("test", "=====index: [" + temp[0] + ", " + temp[1] + "]");
+        }
     }
 }
