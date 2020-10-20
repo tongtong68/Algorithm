b8085284c5cde6fda70781deba6d18eb90c4b65c
Carter Peng <pengtong@infosec.com.cn>
2020-10-20 13:49:29 +0800

    第十九题，删除链表的倒数第n个节点

---
+++ app/src/main/java/com/example/algorithm/medium/Nineteen.java
@@ -0,0 +1,60 @@
+package com.example.algorithm.medium;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Nineteen {
+
+    // 方法一：计算链表长度
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        int length = getLength(head);
+        ListNode cur = dummy;
+        for (int i = 1; i < length - n + 1; ++i) {
+            cur = cur.next;
+        }
+        cur.next = cur.next.next;
+        ListNode ans = dummy.next;
+        return ans;
+    }
+
+    private int getLength(ListNode head) {
+        int length = 0;
+        while (head != null) {
+            ++length;
+            head = head.next;
+        }
+        return length;
+    }
+
+    // 方法二：栈
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        Deque<ListNode> stack = new LinkedList<ListNode>();
+        ListNode cur = dummy;
+        while (cur != null) {
+            stack.push(cur);
+            cur = cur.next;
+        }
+        for (int i = 0; i < n; ++i) {
+            stack.pop();
+        }
+        ListNode prev = stack.peek();
+        prev.next = prev.next.next;
+        ListNode ans = dummy.next;
+        return ans;
+    }
+
+    class ListNode {
+        int value;
+        ListNode next;
+        ListNode(int value) {
+            this.value = value;
+        }
+
+        public ListNode(int value, ListNode next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
