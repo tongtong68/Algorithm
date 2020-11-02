98ce2ea6d6a08ca3933098aa372acffa88566ac9
Carter Peng <pengtong@infosec.com.cn>
2020-11-02 14:17:40 +0800

    第四十八题，方法二、三还没看懂

---
+++ app/src/main/java/com/example/algorithm/medium/FortyEight.java
@@ -0,0 +1,68 @@
+package com.example.algorithm.medium;
+
+public class FortyEight {
+
+    // 转置加翻转
+    // 时间复杂度 O(n2)
+    // 空间复杂度 O(1)
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // transpose matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int tmp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = tmp;
+            }
+        }
+        // reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = tmp;
+            }
+        }
+    }
+
+    // 旋转四个矩形
+    // 时间复杂度：O(N^2)
+    // 空间复杂度：O(1)
+    public void rotate2(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2 + n % 2; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int[] tmp = new int[4];
+                int row = i;
+                int col = j;
+                for (int k = 0; k < 4; k++) {
+                    tmp[k] = matrix[row][col];
+                    int x = row;
+                    row = col;
+                    col = n - 1 - x;
+                }
+                for (int k = 0; k < 4; k++) {
+                    matrix[row][col] = tmp[(k + 3) % 4];
+                    int x = row;
+                    row = col;
+                    col = n - 1 - x;
+                }
+            }
+        }
+    }
+
+    // 在单次循环中旋转 4 个矩形
+    public void rotate3(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < (n + 1) / 2; i ++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }
+}
