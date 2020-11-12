5fd0e68fbdcbdd28a4bc124e50e735ac185f4402
Carter Peng <pengtong@infosec.com.cn>
2020-11-12 11:22:17 +0800

    第七十六题，七十七题，目前都没有看太明白，主要的问题还是递归有点难以理解

---
+++ app/src/main/java/com/example/algorithm/difficult/SeventySix.java
@@ -0,0 +1,59 @@
+package com.example.algorithm.difficult;
+
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class SeventySix {
+    Map<Character, Integer> original = new HashMap<Character, Integer>();
+    Map<Character, Integer> cnt = new HashMap<Character, Integer>();
+
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public String minWindow(String s, String t) {
+        int tLen = t.length();
+        for (int i = 0; i < tLen; i++) {
+            char c = t.charAt(i);
+            original.put(c, original.getOrDefault(c, 0) + 1);
+        }
+        int l = 0, r = -1;
+        int len = Integer.MAX_VALUE, ansL = -1, ansR = -1;
+        int sLen = s.length();
+        while (r < sLen) {
+            ++r;
+            if (r < sLen && original.containsKey(s.charAt(r))) {
+                cnt.put(s.charAt(r), cnt.getOrDefault(s.charAt(r), 0) + 1);
+            }
+            while (check() && l <= r) {
+                if (r - l + 1 < len) {
+                    len = r - l + 1;
+                    ansL = l;
+                    ansR = l + len;
+                }
+                if (original.containsKey(s.charAt(l))) {
+                    cnt.put(s.charAt(l), cnt.getOrDefault(s.charAt(l), 0) - 1);
+                }
+                ++l;
+            }
+        }
+        return ansL == -1 ? "" : s.substring(ansL, ansR);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public boolean check() {
+        Iterator iter = original.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry entry = (Map.Entry) iter.next();
+            Character key = (Character) entry.getKey();
+            Integer val = (Integer) entry.getValue();
+            if (cnt.getOrDefault(key, 0) < val) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
