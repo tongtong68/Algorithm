d03bf42c15a048c53cca3341edf06c94819e81c6
Carter Peng <pengtong@infosec.com.cn>
2020-11-23 14:24:10 +0800

    第121题，下次要自己写出来

---
+++ app/src/main/java/com/example/algorithm/easy/HundredTwentyOne.java
@@ -0,0 +1,35 @@
+package com.example.algorithm.easy;
+
+public class HundredTwentyOne {
+
+
+    // 时间复杂度：O(n^2)
+    // 空间复杂度：O(1)
+    public int maxProfit(int prices[]) {
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                int profit = prices[j] - prices[i];
+                if (profit > maxprofit) {
+                    maxprofit = profit;
+                }
+            }
+        }
+        return maxprofit;
+    }
+
+    // 时间复杂度：O(n)
+    // 空间复杂度：O(1)
+    public int maxProfit2(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice) {
+                minprice = prices[i];
+            } else if (prices[i] - minprice > maxprofit) {
+                maxprofit = prices[i] - minprice;
+            }
+        }
+        return maxprofit;
+    }
+}
