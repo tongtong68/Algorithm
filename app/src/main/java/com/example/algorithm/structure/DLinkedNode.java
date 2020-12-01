67a2495e8614df68fea8d8944fea357840fe7eaa
Carter Peng <pengtong@infosec.com.cn>
2020-12-01 11:51:48 +0800

    第146题，实现了一个简单的LRUCache. java内部实现是一个linkedHashMap

---
+++ app/src/main/java/com/example/algorithm/structure/DLinkedNode.java
@@ -0,0 +1,18 @@
+package com.example.algorithm.structure;
+
+public class DLinkedNode {
+
+    public int key;
+    public int value;
+    public DLinkedNode prev;
+    public DLinkedNode next;
+
+    public DLinkedNode(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public DLinkedNode() {
+
+    }
+}
