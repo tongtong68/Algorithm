bc8e4ab80effad62d1ee5e0412a5561b53c74c9b
Carter Peng <pengtong@infosec.com.cn>
2020-12-02 14:21:50 +0800

    第155题，利用辅助栈的思想存储当前栈的最小值

---
+++ app/src/main/java/com/example/algorithm/difficult/HundredAndFiftyFive.java
@@ -0,0 +1,34 @@
+package com.example.algorithm.difficult;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class HundredAndFiftyFive {
+
+    Deque<Integer> xStack;
+    Deque<Integer> minStack;
+
+    public HundredAndFiftyFive() {
+        xStack = new LinkedList<Integer>();
+        minStack = new LinkedList<Integer>();
+        minStack.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int x) {
+        xStack.push(x);
+        minStack.push(Math.min(minStack.peek(), x));
+    }
+
+    public void pop() {
+        xStack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return xStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
