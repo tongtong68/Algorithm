a6b58a07c2144b3b71a1410c45b1c3a10df2c7b8
Carter Peng <pengtong@infosec.com.cn>
2020-12-04 11:01:26 +0800

    第198题，使用动态规划解决问题

---
+++ app/src/main/java/com/example/algorithm/easy/HundredAndNinetyEight.java
@@ -0,0 +1,47 @@
+package com.example.algorithm.easy;
+
+public class HundredAndNinetyEight {
+
+    public int[] nums = new int[]{2, 7, 9, 3, 1};
+
+    // 动态规划
+    // 时间复杂度O(N)
+    // 空间复杂度O(N)
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[length - 1];
+    }
+
+
+    // 动态规划+滚动数组
+    // 时间复杂度O(N)
+    // 空间复杂度O(1)
+    public int rob2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        }
+        int first = nums[0], second = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < length; i++) {
+            int temp = second;
+            second = Math.max(first + nums[i], second);
+            first = temp;
+        }
+        return second;
+    }
+}
