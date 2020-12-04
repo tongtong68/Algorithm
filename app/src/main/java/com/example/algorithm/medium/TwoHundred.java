9b8cb96bd8d7770d895c981df252bf511899da36
Carter Peng <pengtong@infosec.com.cn>
2020-12-04 14:13:57 +0800

    第200题，图的遍历算法

---
+++ app/src/main/java/com/example/algorithm/medium/TwoHundred.java
@@ -0,0 +1,94 @@
+package com.example.algorithm.medium;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TwoHundred {
+
+    int[][] grid = new int[][]{{1, 0, 1}, {2, 3, 4}, {3, 4, 5}};
+
+    void dfs(char[][] grid, int row, int column) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+
+        if (row < 0 || column < 0 || row >= nr || column >= nc || grid[row][column] == '0') {
+            return;
+        }
+
+        grid[row][column] = '0';
+        dfs(grid, row - 1, column);
+        dfs(grid, row + 1, column);
+        dfs(grid, row, column - 1);
+        dfs(grid, row, column + 1);
+    }
+
+    // 方法一：深度优先搜索
+    // 时间复杂度：O(MN)
+    // 空间复杂度：O(MN)
+    public int numIslands1(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+
+        return num_islands;
+    }
+
+    // 方法二：广度优先搜索
+    // 时间复杂度：O(MN)
+    // 空间复杂度：O(min(M,N))
+    public int numIslands2(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    grid[r][c] = '0';
+                    Queue<Integer> neighbors = new LinkedList<>();
+                    neighbors.add(r * nc + c);
+                    while (!neighbors.isEmpty()) {
+                        int id = neighbors.remove();
+                        int row = id / nc;
+                        int col = id % nc;
+                        if (row - 1 >= 0 && grid[row-1][col] == '1') {
+                            neighbors.add((row-1) * nc + col);
+                            grid[row-1][col] = '0';
+                        }
+                        if (row + 1 < nr && grid[row+1][col] == '1') {
+                            neighbors.add((row+1) * nc + col);
+                            grid[row+1][col] = '0';
+                        }
+                        if (col - 1 >= 0 && grid[row][col-1] == '1') {
+                            neighbors.add(row * nc + col-1);
+                            grid[row][col-1] = '0';
+                        }
+                        if (col + 1 < nc && grid[row][col+1] == '1') {
+                            neighbors.add(row * nc + col+1);
+                            grid[row][col+1] = '0';
+                        }
+                    }
+                }
+            }
+        }
+
+        return num_islands;
+    }
+}
