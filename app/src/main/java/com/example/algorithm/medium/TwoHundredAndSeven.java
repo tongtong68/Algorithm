da8e1ed190b30fc03d2f19e1e68e7fb0593baccd
Carter Peng <pengtong@infosec.com.cn>
2020-12-08 15:48:28 +0800

    第207题，主要是图的遍历算法

---
+++ app/src/main/java/com/example/algorithm/medium/TwoHundredAndSeven.java
@@ -0,0 +1,46 @@
+package com.example.algorithm.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TwoHundredAndSeven {
+    List<List<Integer>> edges;
+    int[] visited;
+    boolean valid = true;
+
+    // 方法一：深度优先搜索
+    // 时间复杂度: O(n+m)
+    // 空间复杂度: O(n+m)
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        edges = new ArrayList<List<Integer>>();
+        for (int i = 0; i < numCourses; ++i) {
+            edges.add(new ArrayList<Integer>());
+        }
+        visited = new int[numCourses];
+        for (int[] info : prerequisites) {
+            edges.get(info[1]).add(info[0]);
+        }
+        for (int i = 0; i < numCourses && valid; ++i) {
+            if (visited[i] == 0) {
+                dfs(i);
+            }
+        }
+        return valid;
+    }
+
+    public void dfs(int u) {
+        visited[u] = 1;
+        for (int v: edges.get(u)) {
+            if (visited[v] == 0) {
+                dfs(v);
+                if (!valid) {
+                    return;
+                }
+            } else if (visited[v] == 1) {
+                valid = false;
+                return;
+            }
+        }
+        visited[u] = 2;
+    }
+}
