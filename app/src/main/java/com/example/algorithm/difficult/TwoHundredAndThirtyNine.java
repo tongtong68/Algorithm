6f2b4681cdae9081ed71a475e4462f9c6a891590
Carter Peng <pengtong@infosec.com.cn>
2020-12-14 10:31:31 +0800

    第239题，动态规划方法没怎么看懂

---
+++ app/src/main/java/com/example/algorithm/difficult/TwoHundredAndThirtyNine.java
@@ -0,0 +1,52 @@
+package com.example.algorithm.difficult;
+
+public class TwoHundredAndThirtyNine {
+
+    // 暴力法
+    // 时间复杂度：O(Nk)
+    // 空间复杂度：O(N−k+1)
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        if (n * k == 0) return new int[0];
+
+        int [] output = new int[n - k + 1];
+        for (int i = 0; i < n - k + 1; i++) {
+            int max = Integer.MIN_VALUE;
+            for(int j = i; j < i + k; j++)
+                max = Math.max(max, nums[j]);
+            output[i] = max;
+        }
+        return output;
+    }
+
+
+    // 方法三: 动态规划
+    // 时间复杂度：O(N)
+    // 空间复杂度：O(N)
+    public int[] maxSlidingWindow2(int[] nums, int k) {
+        int n = nums.length;
+        if (n * k == 0) return new int[0];
+        if (k == 1) return nums;
+
+        int [] left = new int[n];
+        left[0] = nums[0];
+        int [] right = new int[n];
+        right[n - 1] = nums[n - 1];
+        for (int i = 1; i < n; i++) {
+            // from left to right
+            if (i % k == 0) left[i] = nums[i];  // block_start
+            else left[i] = Math.max(left[i - 1], nums[i]);
+
+            // from right to left
+            int j = n - i - 1;
+            if ((j + 1) % k == 0) right[j] = nums[j];  // block_end
+            else right[j] = Math.max(right[j + 1], nums[j]);
+        }
+
+        int [] output = new int[n - k + 1];
+        for (int i = 0; i < n - k + 1; i++)
+            output[i] = Math.max(left[i + k - 1], right[i]);
+
+        return output;
+    }
+}
