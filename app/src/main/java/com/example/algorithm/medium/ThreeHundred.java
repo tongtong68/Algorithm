b746694b04186bd173218c56f7d67e253e13b41f
Carter Peng <pengtong@infosec.com.cn>
2020-12-16 14:19:16 +0800

    第300题，使用动态规划，思路是对的，还是需要自己写出来。 第301题，还没看懂算法

---
+++ app/src/main/java/com/example/algorithm/medium/ThreeHundred.java
@@ -0,0 +1,59 @@
+package com.example.algorithm.medium;
+
+public class ThreeHundred {
+
+    private int[] nums = new int[]{1, 9, 7, 2, 5, 3, 6, 11};
+
+    // 方法一：动态规划
+    // 时间复杂度：O(n^2)
+    // 空间复杂度：O(n)
+    public int getLongestSub(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int maxans = 1;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxans = Math.max(maxans, dp[i]);
+        }
+        return maxans;
+    }
+
+    // 方法二：贪心 + 二分查找
+    // 时间复杂度：O(nlogn)
+    // 空间复杂度：O(n)
+    public int lengthOfLIS(int[] nums) {
+        int len = 1, n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] d = new int[n + 1];
+        d[len] = nums[0];
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] > d[len]) {
+                d[++len] = nums[i];
+            } else {
+                int l = 1, r = len, pos = 0; // 如果找不到说明所有的数都比 nums[i] 大，此时要更新 d[1]，所以这里将 pos 设为 0
+                while (l <= r) {
+                    int mid = (l + r) >> 1;
+                    if (d[mid] < nums[i]) {
+                        pos = mid;
+                        l = mid + 1;
+                    } else {
+                        r = mid - 1;
+                    }
+                }
+                d[pos + 1] = nums[i];
+            }
+        }
+        return len;
+    }
+
+}
